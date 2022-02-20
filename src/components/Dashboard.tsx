ce7abb86919773631147d5d57cdf8f0ea81a5675
Dichiara25 <edclrcq@gmail.com>
2022-02-20 22:46:51 +0100

    Added a not-connected container and an unworking airdrop button

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -3,14 +3,26 @@ import { Emergency } from './DashboardComponents/Emergency';
 import { Claim } from './DashboardComponents/Claim';
 import { Pending } from './DashboardComponents/Pending';
 import { Tab, Tabs } from 'react-bootstrap';
+import { useWallet } from '@solana/wallet-adapter-react';
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Dashboard.css';
+import './DashboardComponents/Common.css';
 
 export const Dashboard: FC = () => {
-    return (
-        <div className='dashboard-container'>
+    const { publicKey } = useWallet();
 
+    const renderWalletNotConnected = () => {
+        return (
+            <div className='not-connected-container'>
+                <p>Think of tomorrow.</p>
+                <WalletMultiButton />
+            </div>
+        )
+    }
+
+    const renderWalletConnected = () => {
+        return (
             <Tabs defaultActiveKey="emergency" id="tabs" className="mb-3">
                 <Tab eventKey="emergency" title="Emergency list" className='tab-content'>
                     <Emergency />
@@ -22,6 +34,13 @@ export const Dashboard: FC = () => {
                     <Pending />
                 </Tab>
             </Tabs>
+        )
+    }
+
+    return (
+        <div className='dashboard-container'>
+            {!publicKey && renderWalletNotConnected()}
+            {publicKey && renderWalletConnected()}
         </div>
     )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,37 @@
-import React, { FC } from 'react';
-import { Container, Nav, Navbar } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
+import { WalletNotConnectedError } from '@solana/wallet-adapter-base';
+import { useConnection, useWallet } from '@solana/wallet-adapter-react';
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
-
-import "./Header.css";
+import { clusterApiUrl, Connection, LAMPORTS_PER_SOL } from '@solana/web3.js';
+import React, { FC, useCallback, useState } from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import './DashboardComponents/Common.css';
+import './Header.css';
 
 export const Header: FC = () => {
-    return(
+    const { connection } = useConnection();
+    const { publicKey } = useWallet();
+    const [airdropping, setAirdropping] = useState(false);
+
+    const claimAirdrop = useCallback(() => {
+        if (airdropping) return;
+
+        setAirdropping(true);
+        if (!publicKey) throw new WalletNotConnectedError();
+
+        let connection = new Connection(clusterApiUrl('testnet'));
+
+        connection
+            .requestAirdrop(publicKey, LAMPORTS_PER_SOL)
+            .then((confirmation) => connection.confirmTransaction(confirmation, 'processed'))
+            .catch(() => alert('Airdrop failed!'))
+            .finally(() => setAirdropping(false));
+    }, [publicKey, connection]);
+
+    return (
         <Navbar expand="lg" variant="dark" fixed="top">
             <Container>
-                <Navbar.Brand className = "fs-3" href="/">
+                <Navbar.Brand className="fs-3" href="/">
                     <p>Notariz</p>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -22,9 +44,14 @@ export const Header: FC = () => {
                             <p>About</p>
                         </NavLink>
                     </Nav>
-                    <WalletMultiButton />
-                </Navbar.Collapse> 
+                    {publicKey && (
+                        <button className="cta-button airdrop-button" onClick={claimAirdrop} disabled={airdropping}>
+                            Airdrop
+                        </button>
+                    )}
+                    <WalletMultiButton className="cta-button" />
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     );
-}
+};
